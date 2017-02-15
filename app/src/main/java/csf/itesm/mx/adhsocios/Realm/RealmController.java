b183cbbfb845a4bf291f7412c47924115ce7f032
rubcuadra <rubcuadra@gmail.com>
2017-02-15 09:44:51 -0600

    realm

--- app/src/main/java/csf/itesm/mx/adhsocios/Realm/RealmController.java
+++ app/src/main/java/csf/itesm/mx/adhsocios/Realm/RealmController.java
@@ -17,13 +17,15 @@ public class RealmController
     private static RealmController instance;
     private final Realm realm;
 
-    public RealmController(Application application) {
+    public RealmController(Application application)
+    {
         realm = Realm.getDefaultInstance();
     }
 
-    public static RealmController with(Fragment fragment) {
-
-        if (instance == null) {
+    public static RealmController with(Fragment fragment)
+    {
+        if (instance == null)
+        {
             instance = new RealmController(fragment.getActivity().getApplication());
         }
         return instance;
